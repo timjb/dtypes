a250b4caee97195250b55a6c0e37b26533640a04
Tim Baumann <tim@timbaumann.info>
2016-07-13 22:54:07 +0200

    add 'fap' function (yeah...)

--- src/FTypes/Classes/FMonad.hs
+++ src/FTypes/Classes/FMonad.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE TypeOperators #-}
 
 module FTypes.Classes.FMonad
   ( FMonad (..)
   , Diag (..)
+  , fap
   ) where
 
 import FTypes.Classes.FApplicative
 import FTypes.Classes.FFunctor
 import FTypes.Compose
+import FTypes.Trafo
 
 newtype Diag f a
   = Diag
@@ -22,3 +25,14 @@ class FApplicative r => FMonad (r :: (k -> *) -> *) where
   fjoin x = fbind x getCompose
   fbind :: r f -> (forall a. f a -> r (g a)) -> r (Diag g)
   fbind x f = fjoin (Compose . f <<$>> x)
+
+data FBiCoyonedaLike f g a b
+  = FBiCoyonedaLike (f a -> g a) (f b)
+
+fap :: FMonad r => r (f ==>> g) -> r f -> r g
+fap x y =
+  joinFBiCoyonedaLike <<$>>
+  fbind x (\t -> (FBiCoyonedaLike (unTrafoComp t) <<$>> y))
+  where
+    joinFBiCoyonedaLike :: Diag (FBiCoyonedaLike f g) a -> g a
+    joinFBiCoyonedaLike (Diag (FBiCoyonedaLike f z)) = f z
